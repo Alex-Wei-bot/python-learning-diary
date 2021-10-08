028275ed7db1d4c91cf607a8da33a750a2e89e11
走水 <zhaotaoalex@outlook.com>
2021-10-09 00:07:05 +0800

    Add files via upload

---
+++ exercise code/day1.py
@@ -0,0 +1,15 @@
+#turtle drawing
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('green')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()
